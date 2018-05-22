dac473992ccb815a3a5cbe39c8a5e59dad77bc2d
Robbie Jaeger <jaeger.rob@gmail.com>
2018-05-22 09:18:21 -0600

    Add dotenv configuration

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
+
 
 app.listen(app.get('port'), () => {
   console.log('Practice oauth server for github');
