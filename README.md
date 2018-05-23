ca0a7f0e8a235e77cd37c9211efdff95cab0146b
Robbie Jaeger <jaeger.rob@gmail.com>
2018-05-23 11:00:26 -0600

    Add readme and refactor

---
+++ README.md
@@ -0,0 +1,5 @@
+# GitHub OAuth Practice
+
+This is a practice exercise in implementing OAuth 2.0 using [GitHub's OAuth strategy](https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/).
+
+By no means is this secure, but an experiment in getting OAuth tokens from scratch.
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/login', (request, response) => {
   const clientID = process.env.GITHUB_CLIENT_ID;
   // redirect URI, using the defualt callback URL in app registration
   const scope = 'repo';
-  const state = 'unguessablestring';
+  const state = process.env.GITHUB_STATE;
 
   response.json({clientID, scope, state}).status(200);
 });
@@ -22,23 +22,30 @@ app.get('/callback', (request, response) => {
   const clientID = process.env.GITHUB_CLIENT_ID;
   const clientSecret = process.env.GITHUB_CLIENT_SECRET;
   const code = request.query.code;
-  const state = request.query.state; // should check to see if this matches
+  const state = request.query.state;
 
+  if (state === process.env.GITHUB_STATE) {
+    getUserToken(response, clientID, clientSecret, code);
+  } else {
+    return response.redirect('/');
+  }
+});
+
+const getUserToken = (response, clientID, clientSecret, code) => {
   fetch(`https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${code}`, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
     .then(response => response.json())
     .then(json => {
       console.log(json)
-      response.redirect('/');
+      return response.redirect('/'); // and add token to a cookie?
     })
     .catch(err => console.log(err));
-
-});
+}
 
 app.listen(app.get('port'), () => {
-  console.log('Practice oauth server for github');
+  console.log('Practice OAuth server for GitHub running.');
 });
